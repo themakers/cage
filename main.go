2ec1eec6f52feac77c28f3212f9b2bc5e3a27353
The Architect <thearchitect@themake.rs>
2026-02-03 02:30:08 +0300

    release action; upx!

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "log/slog"
-    "os"
-    "strings"
+	"flag"
+	"fmt"
+	"log/slog"
+	"os"
+	"strings"
 )
 
 // TODO: rm command
@@ -22,65 +22,65 @@ import (
 // TODO: tpm
 
 func main() {
-    logger := newLogger()
+	logger := newLogger()
 
-    global := flag.NewFlagSet("cage", flag.ContinueOnError)
-    global.SetOutput(os.Stderr)
-    wd := global.String("wd", "", "cage root (directory containing .cage); if omitted, discover from CWD")
-    if err := global.Parse(os.Args[1:]); err != nil {
-        exitErr(err)
-    }
-    args := global.Args()
-    if len(args) == 0 {
-        printUsage(os.Stderr)
-        os.Exit(2)
-    }
+	global := flag.NewFlagSet("cage", flag.ContinueOnError)
+	global.SetOutput(os.Stderr)
+	wd := global.String("wd", "", "cage root (directory containing .cage); if omitted, discover from CWD")
+	if err := global.Parse(os.Args[1:]); err != nil {
+		exitErr(err)
+	}
+	args := global.Args()
+	if len(args) == 0 {
+		printUsage(os.Stderr)
+		os.Exit(2)
+	}
 
-    cmd := args[0]
-    cmdArgs := args[1:]
+	cmd := args[0]
+	cmdArgs := args[1:]
 
-    switch cmd {
-    case "encrypt":
-        exitErr(cmdEncrypt(logger, *wd, cmdArgs))
-    case "decrypt":
-        exitErr(cmdDecrypt(logger, *wd, cmdArgs))
-    case "dump":
-        exitErr(cmdDump(logger, *wd, cmdArgs))
-    case "run":
-        exitErr(cmdRun(logger, *wd, cmdArgs))
-    case "init":
-        exitErr(cmdInit(logger, *wd, cmdArgs))
-    case "-h", "--help", "help":
-        printUsage(os.Stdout)
-        return
-    default:
-        printUsage(os.Stderr)
-        os.Exit(2)
-    }
+	switch cmd {
+	case "encrypt":
+		exitErr(cmdEncrypt(logger, *wd, cmdArgs))
+	case "decrypt":
+		exitErr(cmdDecrypt(logger, *wd, cmdArgs))
+	case "dump":
+		exitErr(cmdDump(logger, *wd, cmdArgs))
+	case "run":
+		exitErr(cmdRun(logger, *wd, cmdArgs))
+	case "init":
+		exitErr(cmdInit(logger, *wd, cmdArgs))
+	case "-h", "--help", "help":
+		printUsage(os.Stdout)
+		return
+	default:
+		printUsage(os.Stderr)
+		os.Exit(2)
+	}
 }
 
 func newLogger() *slog.Logger {
-    level := slog.LevelInfo
-    if s := strings.TrimSpace(os.Getenv("CAGE_LOG_LEVEL")); s != "" {
-        s = strings.ToLower(s)
-        switch s {
-        case "debug":
-            level = slog.LevelDebug
-        case "warn", "warning":
-            level = slog.LevelWarn
-        case "error":
-            level = slog.LevelError
-        case "info":
-            level = slog.LevelInfo
-        }
-    }
-    return slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: level}))
+	level := slog.LevelInfo
+	if s := strings.TrimSpace(os.Getenv("CAGE_LOG_LEVEL")); s != "" {
+		s = strings.ToLower(s)
+		switch s {
+		case "debug":
+			level = slog.LevelDebug
+		case "warn", "warning":
+			level = slog.LevelWarn
+		case "error":
+			level = slog.LevelError
+		case "info":
+			level = slog.LevelInfo
+		}
+	}
+	return slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: level}))
 }
 
 func exitErr(err error) {
-    if err == nil {
-        return
-    }
-    fmt.Fprintln(os.Stderr, "error:", err)
-    os.Exit(1)
+	if err == nil {
+		return
+	}
+	fmt.Fprintln(os.Stderr, "error:", err)
+	os.Exit(1)
 }
